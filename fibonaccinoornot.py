3b8120835650e7d0a85e71095e773af961768e56
Karnam Nithin <98379951+nithinkarnam@users.noreply.github.com>
2023-02-26 10:24:35 +0530

    Create fibonaccinoornot.py

---
+++ fibonaccinoornot.py
@@ -0,0 +1,16 @@
+n=int(input("enter the number to check if it is A fibonacci no")
+c=0
+a=1
+b=1
+if n==0 or n==1:
+  print("yes")
+else:
+      while c<n
+      c=a+b
+      b=a
+      a=c
+      if c==n:
+      print("yes")
+      else:
+      print("no")
+      
